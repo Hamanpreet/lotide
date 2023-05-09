9b1bfb5d565c2655bc3c3c931296c73e0de3af9a
Hamanpreet <haman42626@gmail.com>
2023-05-09 17:04:39 -0400

    Separated Test code

---
+++ test/assertEqualTest.js
@@ -0,0 +1,3 @@
+const assertEqual = require("../assertEqual.js")
+assertEqual("Lighthouse","Bootcamp");
+assertEqual(1,4);
---
+++ test/headTest.js
@@ -0,0 +1,5 @@
+const head = require("../head.js");
+const assertEqual = require("../assertEqual.js");
+
+assertEqual(head([1, 2, 3, 4, 5]), 5);
+assertEqual(head(["Hello", "World"]), "Hello");
