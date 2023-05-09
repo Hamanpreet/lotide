f99f79488707543f741c95467068fc24b911ed4b
Hamanpreet <haman42626@gmail.com>
2023-05-09 17:53:48 -0400

    Refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,6 @@
 const eqArrays = function(arr1, arr2) {
-  for(let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
       return false;
     }
   }
