26d1c92f8ba0fb5f3a61d3e2d4b13ba55d43bf3b
Hamanpreet <haman42626@gmail.com>
2023-05-09 17:30:37 -0400

    Refactored Tail

--- tail.js
+++ tail.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    console.log(`😁Assertion Passed: ${actual} === ${expected}`);
-  else
-    console.log(`😒undefined`);
-};
 const tail = function(arr) {
   let newArr = [];
   for (let i = 1; i < arr.length; i++) {
@@ -12,7 +6,4 @@ const tail = function(arr) {
   return newArr;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(result);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+module.exports = tail;
